6c8859b5142f2a469c85e9b707fff0f243f5ac2b
Georgvit <ifreyd@mail.ru>
2023-09-11 00:12:14 +0300

    Initial commit

---
+++ src/DatabaseAccess.java
@@ -0,0 +1,11 @@
+import java.util.Collection;
+
+/**
+ * Интерфейс DAC
+ */
+public interface DatabaseAccess {
+    void addEntity(Entity entity);
+    void removeEntity(Entity entity);
+    Collection<Texture> getAllTextures();
+    Collection<Model3D> getAllModels();
+}
---
+++ src/UILayer.java
@@ -0,0 +1,11 @@
+public interface UILayer {
+    void openProject(String fileName);
+    void showProjectSettings();
+    void saveProject();
+    void printAllModels();
+    void printAllTextures();
+    void renderAll();
+    void dellModels(int i);
+    void addModel();
+    void renderModel(int i);
+}
